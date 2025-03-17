3211b96ad56f0435c491fdf1b6d2c3f9db4150b2
ian luddy <ianluddy@gmail.com>
2025-03-17 16:12:14 +0000

    Mobile friendly bot styling

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -45,6 +45,17 @@ const ChatWindow = styled.div`
   display: flex;
   flex-direction: column;
   overflow: hidden;
+
+  @media (max-width: 768px) {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    border-radius: 0;
+    box-shadow: none;
+    bottom: 0;
+    right: 0;
+    z-index: 1000;
+  }
 `
 
 const ChatHeader = styled.div`
